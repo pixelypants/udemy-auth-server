1247e5a5ba794e1194d3659284acddc9c35f6676
Hudson Graham <hudson.graham@tattsgroup.com>
2019-02-04 19:16:52 +1000

    init commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+
+console.log("Server listening on port: ", port)
+
+// https://developer.okta.com/blog/2018/11/15/node-express-typescript
