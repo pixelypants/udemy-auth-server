0ed7da628430d2fd98b92e66c08df450a9611194
Hudson Graham <hudson.graham@gmail.com>
2019-02-06 11:15:36 +1000

    Made connection with mongodb

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ const express = require('express');
 const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const router = require('./router')
 
 const app = express();
+
+const router = require('./router')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true })
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
